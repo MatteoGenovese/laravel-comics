78cf4ce781a49b4b95bcf105c9aca3937e3adbd8
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-07 23:15:59 +0200

    feat: added the data in  comics.php

--- laravel-comics/config/comics.php
+++ laravel-comics/config/comics.php
@@ -13,6 +13,8 @@ class Comic{
 	}
 }
 
+
+
 return [
 	new Comic("Action Comics #1000: The Deluxe Edition", "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX", "comic book"),
 	new Comic("American Vampire 1976 #1", "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01", "comic book"),
