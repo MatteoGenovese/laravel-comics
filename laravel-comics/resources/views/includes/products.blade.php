98b624815fe0f659c5deb80b5fa5820880c072d8
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-08 00:03:04 +0200

    feat: information shown in product-container due to blade language

--- laravel-comics/resources/views/includes/products.blade.php
+++ laravel-comics/resources/views/includes/products.blade.php
@@ -1,5 +1,17 @@
 <div class="product-container">
     <div class="wrapper container">
+@forelse ($products as $product)
+<div class="product" >
+    <div class="image-container">
+       <img src="{{asset( 'images/'.$product->getUrl() )}} " alt="{{$product->getTitle()}}">  
+    </div>
+    <h4> {{ $product->getTitle() }} </h4> 
+  </div>
+@empty
+        <div>prodotti non visualizzabili al momento</div>
+    
+@endforelse
+
 
     </div>
 </div>
