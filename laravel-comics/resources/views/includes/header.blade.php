32e2537f1a1ab2c016ff88467cfc06698c58a906
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-07 23:19:24 +0200

    feat: added links in header dinamically

--- laravel-comics/resources/views/includes/header.blade.php
+++ laravel-comics/resources/views/includes/header.blade.php
@@ -1,13 +1,27 @@
 <header class="d-flex justify-content-between container">
-    <div class="img-container">
-      <img src="../assets/images/dc-logo.png" alt="">
-    </div>
-    <div >
-      <ul class="d-flex align-items-center">
-        <li>ciao</li>
-        <li>ciao</li>
-        <li>ciao</li>
-        <li>ciao</li>
-      </ul>
-    </div>
-  </header>
+  <div class="img-container">
+    <img src="{{ asset('./images/dc-logo.png') }}" alt="dc-logo">
+
+  </div>
+  <div>
+    
+    <ul class="d-flex align-items-center">
+      <li>
+        @forelse ($links as $link)
+      <li 
+      @if($link->isActive==true)
+      class="active" 
+      @endif
+      >
+        {{$link->name}}
+      </li>
+      @empty
+
+      <div> "Non è stato trovato il nav" </div>
+
+      @endforelse
+
+      </li>
+    </ul>
+  </div>
+</header>
